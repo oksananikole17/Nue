e766266b901c913fa1e91133643c08e985137fd0
oksananikole17 <oksananikole17@gmail.com>
2022-10-15 18:33:17 -0400

    Set up Auth CSS

--- app/app.js
+++ app/app.js
@@ -4,6 +4,20 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const authEvents = require('./auth/events.js')
+
 $(() => {
-  // your JS code goes here
+  $('#workout-home').hide()
+  $('#find-modal').hide()
+  $('#workout-display').hide()
+  $('#show-all').hide()
+  $('#sign-out').hide()
+  $('#second').hide()
+  $('#password').hide()
+  $('#show-workout-form').hide()
+  $('#all-workouts').hide()
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#password-form').on('submit', authEvents.onChangePassword)
+  $('#sign-out').on('click', authEvents.onSignOut)
 })
